3f9667b814e4c6891d2cf280c399811a3575fcc3
jordimall <jordi.mallafre@hotmail.com>
2023-12-14 16:06:01 +0100

    Finalización view mostrar un evento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 import { EventComponent } from './event/event.component';
 import { EventListComponent } from './event/event-list/event-list.component';
+import { EventShowComponent } from './event/event-show/event-show.component';
 
 export const routes: Routes = [
   {
     path: 'event',
     component: EventComponent,
     children: [
-      { path: '', component: EventListComponent }
+      { path: '', component: EventListComponent },
+      { path: 'show/:id', component: EventShowComponent },
     ],
   },
 ];
