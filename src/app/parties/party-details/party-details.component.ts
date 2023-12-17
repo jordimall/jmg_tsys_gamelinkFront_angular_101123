03ec5dd00f912f94e15bde36691b0959c165f1ec
Alejandro <alejandro.carayol@gmail.com>
2023-12-17 21:11:25 +0100

    redirección a la vista detallada

--- src/app/parties/party-details/party-details.component.ts
+++ src/app/parties/party-details/party-details.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { Party } from '../../models/party.model';
+import { Game } from '../../models/game.model';
+import { PartyService } from '../../services/party.service';
+import { Router, RouterLink } from '@angular/router';
+import { GameService } from '../../services/game.service';
+import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-party-details',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent, RouterLink],
   templateUrl: './party-details.component.html',
   styleUrl: './party-details.component.css'
 })
 export class PartyDetailsComponent {
 
+  party: Party = new Party;
+  game: Game = new Game;
+
+  constructor(private partyService: PartyService, private gameService: GameService, private route: Router){
+
+  }
+
+  ngOnInit() {
+    let url = this.route.url.split("/");
+    let id = url[url.length-1];
+    this.partyService.getOnePartyById(id as unknown as number).subscribe(
+      result => {
+        this.party = result;
+        console.log(this.party);
+        this.gameService.gameById(this.party.id_game as unknown as number).subscribe(
+          result => {
+            this.game = result
+          },
+          error => {
+            this.route.navigateByUrl("/404");
+          }
+        );
+      },
+      error => {
+
+      }
+    );
+    this.partyService.getMembers(1).subscribe(
+      result => {
+        console.log(result);
+      },
+      error => {
+        console.log("err")
+      }
+    );
+  }
 }
--- src/app/services/party.service.ts
+++ src/app/services/party.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Party } from '../models/party.model';
 import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../models/user.model';
 
-const baseURL = 'http://localhost:3000/party/all?idGame=';
+const baseURL = 'http://localhost:3000/party/';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,14 @@ export class PartyService {
   constructor(private http: HttpClient) {}
 
   allPartiesByGame = (id: number): Observable<Party[]> => {
-    return this.http.get<Party[]>(baseURL+(id as unknown as string));
+    return this.http.get<Party[]>(baseURL+'all?idGame='+(id as unknown as string));
   };
+
+  getOnePartyById = (id: number): Observable<Party> => {
+    return this.http.get<Party>(baseURL+'id/'+id);
+  }
+
+  getMembers = (id: number): Observable<Object[]> => {
+    return this.http.get<Object[]>(baseURL+"members/roles/"+id);
+  }
 }
