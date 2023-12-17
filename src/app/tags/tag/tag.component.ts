a5c305e1f802a0eb0246fce3ec21eab80318d86b
jordimall <jordi.mallafre@hotmail.com>
2023-12-17 16:12:42 +0100

    Finalización listar tag

--- src/app/tags/tag/tag.component.ts
+++ src/app/tags/tag/tag.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Tag } from '../../models/tag.model';
+import { TagService } from '../../services/tag.service';
 @Component({
   selector: 'app-tag',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tag.component.html',
-  styleUrl: './tag.component.css'
+  styleUrl: './tag.component.css',
 })
-export class TagComponent {
+export class TagComponent implements OnInit {
+  numPage: number = 0;
+  tagList: Tag[] | any;
 
+  constructor(private tagService: TagService) {}
+
+  ngOnInit(): void {
+    this.tagService.allTagPaginate(this.numPage).subscribe(
+      (data) => {
+        this.tagList = data;
+      },
+      (err) => {
+        console.log({ status: err.status, message: err.message });
+      }
+    );
+  }
+
+  deleteTag = (id: number): void => {
+    console.log(id);
+  };
 }
