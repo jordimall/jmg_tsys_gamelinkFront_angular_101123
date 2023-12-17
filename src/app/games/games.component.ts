d6808f23da11efe3963169e7a221fa763d356022
Alejandro <alejandro.carayol@gmail.com>
2023-12-17 16:32:23 +0100

    Vista de Games y PartyByGame V1

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
+import { GameService } from '../services/game.service';
+import { Game } from '../models/game.model';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, RouterLink],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css'
 })
-export class GamesComponent {
+export class GamesComponent implements OnInit{
 
+  games: Game[] = [];
+  
+  constructor(private gameService: GameService){
+
+  }
+
+  ngOnInit() {
+    this.gameService.allGame().subscribe(
+      result => {
+        this.games = result;
+      },
+      error => {
+        
+      }
+    )
+  }
 }
