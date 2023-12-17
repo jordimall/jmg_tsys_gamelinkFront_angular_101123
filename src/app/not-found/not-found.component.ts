af302c096d5c6bb47a7449bffb3bb2869f7b411b
Alejandro <alejandro.carayol@gmail.com>
2023-12-17 16:49:13 +0100

    NotFound page creada

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../shared/nav-bar/nav-bar.component';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [NavBarComponent],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+
+}
